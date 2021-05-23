e34b91abed635b44427c87bcfee6cd8d3fde156f
Alexander Greckov <alexander.greckov@gmail.com>
2021-05-23 15:42:46 +0300

    Added permission restrictions for comment model

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -3,23 +3,17 @@ from django.contrib import admin
 from apps.blog.models import Post, Company, Category, Comment
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ('likes', 'dislikes')
-
+class DefaultUserPermissionsMixin:
     def get_queryset(self, request):
         if request.user.is_superuser:
-            return Post.objects.all()
+            return self.model.objects.all()
 
-        return Post.objects.filter(created_by=request.user)
+        return self.model.objects.filter(created_by=request.user)
 
     def has_module_permission(self, request):
         return True
 
     def has_view_permission(self, request, obj=None):
-        if obj is not None:
-            return request.user.is_superuser or request.user == obj.created_by
-
         return True
 
     def has_change_permission(self, request, obj=None):
@@ -44,9 +38,17 @@ class PostAdmin(admin.ModelAdmin):
         return super().save_model(request, obj, form, change)
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(DefaultUserPermissionsMixin, admin.ModelAdmin):
     pass
+
+
+@admin.register(Comment)
+class CommentAdmin(DefaultUserPermissionsMixin, admin.ModelAdmin):
+    readonly_fields = ('post', 'created_by', 'parent')
+
+    def has_add_permission(self, request):
+        return False
 
 
 admin.site.register(Company)
